01683dfe375bb3e62234ba1709de65f4a35e92ea
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2025-12-06 17:20:16 -0600

    Add loader for game-specific script execution

---
+++ loader.lua
@@ -0,0 +1,7 @@
+repeat task.wait() until game:IsLoaded()
+
+
+
+if game.GameId == 7229780065 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/welomenchaina/PureHub/refs/heads/main/Hunted.lua",true))()
+end
